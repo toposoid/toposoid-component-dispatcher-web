047b3381d9c56a6c208ebd4ce692b29ca33b85ff
makoto-kubodera <makoto-kubodera@linked-ideal.com>
2022-01-06 21:14:17 +0900

    https://github.com/toposoid/toposoid-component-dispatcher-web/issues/13

---
+++ test/controllers/HomeControllerSpecEnglish.scala
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2021 Linked Ideal LLC.[https://linked-ideal.com/]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import com.ideal.linked.data.accessor.neo4j.Neo4JAccessor
+import com.ideal.linked.toposoid.knowledgebase.regist.model.Knowledge
+import com.ideal.linked.toposoid.protocol.model.base.AnalyzedSentenceObjects
+import com.ideal.linked.toposoid.sentence.transformer.neo4j.Sentence2Neo4jTransformer
+import org.scalatest.{BeforeAndAfter, BeforeAndAfterAll}
+import org.scalatestplus.play.PlaySpec
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+import play.api.Play.materializer
+import play.api.http.Status.OK
+import play.api.libs.json.Json
+import play.api.test.Helpers.{POST, contentType, defaultAwaitTimeout, status, _}
+import play.api.test.{FakeRequest, _}
+
+class HomeControllerSpecEnglish extends PlaySpec with BeforeAndAfter with BeforeAndAfterAll with GuiceOneAppPerSuite  with Injecting{
+
+  override def beforeAll(): Unit = {
+    Neo4JAccessor.delete()
+    Sentence2Neo4jTransformer.createGraphAuto(List(Knowledge("Life is so comfortable.","en_US", "{}")))
+  }
+
+  override def afterAll(): Unit = {
+    Neo4JAccessor.delete()
+  }
+
+  val controller: HomeController = inject[HomeController]
+  "The specification1" should {
+    "returns an appropriate response" in {
+
+      val json = """{
+                   |    "premise":[],
+                   |    "claim":[{"sentence":"Life is so comfortable.","lang": "en_US", "extentInfoJson":"{}"}]
+                   |}""".stripMargin
+
+      val fr = FakeRequest(POST, "/analyze")
+        .withHeaders("Content-type" -> "application/json")
+        .withJsonBody(Json.parse(json))
+
+      val result = call(controller.analyze(), fr)
+      status(result) mustBe OK
+      contentType(result) mustBe Some("application/json")
+      val jsonResult = contentAsJson(result).toString()
+      val analyzedSentenceObjects: AnalyzedSentenceObjects = Json.parse(jsonResult).as[AnalyzedSentenceObjects]
+      assert(analyzedSentenceObjects.analyzedSentenceObjects.filter(_.deductionResultMap.get("0").get.status).size == 0)
+      assert(analyzedSentenceObjects.analyzedSentenceObjects.filter(_.deductionResultMap.get("1").get.status).size == 1)
+
+    }
+  }
+}
+
